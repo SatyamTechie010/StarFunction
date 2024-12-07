42dd48ca108506b7e8ac0f91609796871eb05300
SatyamTechie010 <sonysahoo548@gmail.com>
2024-12-08 01:13:12 +0530

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import {ghPages} from 'vite-plugin-gh-pages';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: 'Star',
-  plugins: [ghPages()],
-});
+  plugins: [react()],
+})
